ca6b464e82a296369ab8b2990e7f249ad4e69908
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-13 22:16:47 +0200

    cleaned up project. removed standard-components

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+      <!-- Füge hier den Inhalt der Karte ein -->
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Cards',
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    background: #011F35;
+    padding: 20px 32px;
+    gap: 20px;
+    border-radius: 16px 0px 0px 0px;
+  }
+  </style>
